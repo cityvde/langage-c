74c4ebe3a9871fcccd2804ffdc9421798a95001d
cityvde <citynight.fr@gmail.com>
2018-11-13 17:20:24 +0100

    Add files via upload
    Fonction C avec les paramètres optionnels

---
+++ affiche_suite.c
@@ -0,0 +1,20 @@
+﻿#include <stdarg.h>
+#include <stdio.h>
+void affiche_suite (int nb, ...)
+{
+  va_list ap;
+  va_start (ap, nb);
+  while (nb > 0)
+  {
+    int n;
+    n = va_arg (ap, int);
+    printf ("%d.\n", n);
+    --nb;
+  }
+  va_end (ap);
+}
+int main (void)
+{
+  affiche_suite (10, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+  return 0;
+}
